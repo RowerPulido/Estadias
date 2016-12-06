4c594a2ee5023b462a8fbe6d8182c8eb37aa5211
codigashosVictor <victor_1997_sid@hotmail.com>
2016-12-05 17:46:53 -0800

    css tabla de soto y permiso de executar sp

--- Archivos de SQL/usuarios_permisos_proyecto.sql
+++ Archivos de SQL/usuarios_permisos_proyecto.sql
@@ -7,7 +7,6 @@ use master;
 create login alumno
 with password = 'alumno',
 default_database = Estadias; 
-
 use Estadias;
 go
 create user user_tutor from login tutor;
@@ -24,7 +23,11 @@ grant insert on schema :: Estadia to user_tutor;
 grant select on schema :: Documento to user_tutor;
 grant update on schema :: Documento to user_tutor;
 grant insert on schema :: Documento to user_tutor;
-
+grant execute on schema :: Alumno to user_alumno;
+grant execute on schema :: Calificacion to user_alumno;
+grant execute on schema :: Usuario to user_alumno;
+grant execute on schema :: Estadia to user_alumno;
+grant execute on schema :: Documento to user_alumno;
 create user user_alumno from login alumno;
 grant select on schema :: Alumno to user_alumno;
 grant update on schema :: Alumno to user_alumno;

ed2a081f10b43c6885a8f39ec51c3564025d1883
codigashosVictor <victor_1997_sid@hotmail.com>
2016-12-07 17:21:20 -0800

    YIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

--- api/upload.php
+++ api/upload.php
@@ -1,5 +1,6 @@
 <?php 
-header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Origin: *');
+require_once('MODELS/connection_sql_server.php');
 $id=$_POST['id'];
 $matricula=$_POST['matricula'];
 $target_path = "../docs/".$matricula.'/';
@@ -8,7 +9,14 @@ if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path))
 	{
 	 echo '{"status" : 0,
 	 		"Message" : "Upload Succesful",
-	 		"Filename" : "'.$id.'-'.$matricula.'.pdf'.'"}';
+	 		"Filename" : "'.$id.'-'.$matricula.'.pdf" } ';
+
+		$connection= new SqlServerConnection();
+		$query = sprintf('update documento.documentos
+					set status = '."'RECIBIDO'".'
+					where id='.$id.' and alumno=\''.$matricula."'");
+		$connection->execute_query($query);
+		$connection->close();
 	}
 	else
 	{

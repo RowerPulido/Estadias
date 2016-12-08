2a635d80d6d80807165149b39b9f9c0cdfefdeff
codigashosVictor <victor_1997_sid@hotmail.com>
2016-12-07 21:37:20 -0800

    Revert "Revert "Revert "YIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"""
    This reverts commit aeeac5770b4146edc16924776e91132551cba6de.

--- api/upload.php
+++ api/upload.php
@@ -1,6 +1,5 @@
 <?php 
-header('Access-Control-Allow-Origin: *');
-require_once('MODELS/connection_sql_server.php');
+header('Access-Control-Allow-Origin:*');
 $id=$_POST['id'];
 $matricula=$_POST['matricula'];
 $target_path = "../docs/".$matricula.'/';
@@ -9,14 +8,7 @@ if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path))
 	{
 	 echo '{"status" : 0,
 	 		"Message" : "Upload Succesful",
-	 		"Filename" : "'.$id.'-'.$matricula.'.pdf" } ';
-
-		$connection= new SqlServerConnection();
-		$query = sprintf('update documento.documentos
-					set status = '."'RECIBIDO'".'
-					where id='.$id.' and alumno=\''.$matricula."'");
-		$connection->execute_query($query);
-		$connection->close();
+	 		"Filename" : "'.$id.'-'.$matricula.'.pdf'.'"}';
 	}
 	else
 	{
